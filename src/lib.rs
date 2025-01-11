078a940c82aef40c64228cb97a56b9369f68c785
Hovig Manjikian <hovig@mail.com>
2025-01-11 11:49:50 +0100

    Create library project structure

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//! A library that provides basic logging functionality in linux environment.
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
